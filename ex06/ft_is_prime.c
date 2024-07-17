6091d016f9d5c7a476d419c321c141ec0b75df59
kuripa <n.fuhrmann@protonmail.com>
2024-07-17 18:02:16 +0200

    add ex06

---
+++ ex06/ft_is_prime.c
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_is_prime.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nfuhrman <nfuhrman@student.42wolfsburg.de  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/07/17 17:23:09 by nfuhrman          #+#    #+#             */
+/*   Updated: 2024/07/17 17:30:31 by nfuhrman         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+int	prime_checker(int n, int base)
+{
+	if (base % n == 0 || base < 2)
+		return (0);
+	else if (n == base - 1)
+		return (1);
+	else if (base > n)
+		return (prime_checker(n + 1, base));
+	return (1);
+}
+
+int	ft_is_prime(int nb)
+{
+	return (prime_checker(2, nb));
+}
+
