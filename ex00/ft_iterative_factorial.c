eb380ec45e0297be124ab0b1e37805a90fe693f3
kuripa <n.fuhrmann@protonmail.com>
2024-07-17 14:20:54 +0200

    add ex00

---
+++ ex00/ft_iterative_factorial.c
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_iterative_factorial.c                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nfuhrman <nfuhrman@student.42wolfsburg.de  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/07/16 18:16:43 by nfuhrman          #+#    #+#             */
+/*   Updated: 2024/07/16 19:21:14 by nfuhrman         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int	ft_iterative_factorial(int nb)
+{
+	int	i;
+    int	result;
+
+	result = nb; 
+	while (nb > 1)
+	{
+        nb--;
+        result = result * nb ;
+	}
+	return (result);
+}
+
