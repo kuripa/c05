72e9d9e5fe72e7bf383ee5c0f2c45ef4d323afac
kuripa <n.fuhrmann@protonmail.com>
2024-07-18 19:36:43 +0200

    fix: special cases missing

--- ex00/ft_iterative_factorial.c
+++ ex00/ft_iterative_factorial.c
@@ -6,7 +6,7 @@
 /*   By: nfuhrman <nfuhrman@student.42wolfsburg.de  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/16 18:16:43 by nfuhrman          #+#    #+#             */
-/*   Updated: 2024/07/16 19:21:14 by nfuhrman         ###   ########.fr       */
+/*   Updated: 2024/07/18 19:36:02 by nfuhrman         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,11 +14,15 @@ int	ft_iterative_factorial(int nb)
 {
 	int	result;
 
-	result = nb;
+	if (nb < 0)
+		return (0);
+	if (nb == 0)
+		return (1);
+	result = 1;
 	while (nb > 1)
 	{
-		nb--;
 		result = result * nb;
+		nb--;
 	}
 	return (result);
 }
